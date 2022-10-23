dae995342d54554b6507d70fb91ec1a7d0235f8b
DashLee <lsj7915@naver.com>
2022-10-23 16:11:14 +0900

    FVector 형변환 연산자 추가

--- Source/GameAIProgram/Define/Vector2d.h
+++ Source/GameAIProgram/Define/Vector2d.h
@@ -12,6 +12,8 @@ namespace GameAI
 		FVector2d& operator=(const UE::Math::TVector<double>& InValue);
 		FVector2d& operator=(const UE::Math::TVector2<double>& InValue);
 
+		operator FVector() const;
+		
 		void Truncate(double InMax);
 		FVector2d Perp() const;
 	};
